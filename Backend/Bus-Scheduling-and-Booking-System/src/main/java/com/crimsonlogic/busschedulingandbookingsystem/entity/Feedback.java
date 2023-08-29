27af48085f9e66cbec65dcdbf59de8f6cd23f0c5
Rohit Jadhav <rohitjadhav@crimsonlogic.com>
2023-08-29 15:52:36 +0530

    Added Relationship and Validations

--- Backend/Bus-Scheduling-and-Booking-System/src/main/java/com/crimsonlogic/busschedulingandbookingsystem/entity/Feedback.java
+++ Backend/Bus-Scheduling-and-Booking-System/src/main/java/com/crimsonlogic/busschedulingandbookingsystem/entity/Feedback.java
@@ -1,13 +1,29 @@
 package com.crimsonlogic.busschedulingandbookingsystem.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.annotations.ManyToAny;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Table(name = "feedback_info")
+@Getter
+@Setter
+@AllArgsConstructor
 public class Feedback {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +35,10 @@ public class Feedback {
     
     @Column(name = "rating")
     private int rating;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userID_fk")
+    private User user;
     
     
     //default constructors
