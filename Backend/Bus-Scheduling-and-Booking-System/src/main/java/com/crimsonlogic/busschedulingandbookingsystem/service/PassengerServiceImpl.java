060c7dc74d7191b569ade1309f3b6c56f7a4f220
naveenkumar-i <naveenkumar@crimsonlogic.com>
2023-08-29 12:02:13 +0530

    Done with Passenger CRUD Apis

---
+++ Backend/Bus-Scheduling-and-Booking-System/src/main/java/com/crimsonlogic/busschedulingandbookingsystem/service/PassengerServiceImpl.java
@@ -0,0 +1,43 @@
+package com.crimsonlogic.busschedulingandbookingsystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crimsonlogic.busschedulingandbookingsystem.entity.Passenger;
+import com.crimsonlogic.busschedulingandbookingsystem.repository.IPassengerRepository;
+
+
+@Service
+public class PassengerServiceImpl implements IPassengerService{
+    @Autowired
+    private IPassengerRepository passRepo;
+    
+    @Override
+    public List<Passenger> getAllPassengers() {
+        return passRepo.findAll();
+    }
+    
+    @Override
+    public Passenger getPassengerById(Integer id) {
+        return passRepo.findById(id).get();
+    }
+    
+    @Override
+    public Passenger createPassenger(Passenger passenger) {
+        return passRepo.save(passenger);
+    }
+   
+    @Override
+    public Passenger updatePassengerbyId(Integer passengerId, Passenger passenger) {
+        getPassengerById(passengerId);
+        passenger.setPassengerId(passengerId);
+        return passRepo.save(passenger);
+    }
+    
+    @Override
+    public void deletePassengerbyId(Integer id) {
+    	passRepo.deleteById(id);
+    }
+}
