83f0d0d408aa0e053e026a3735be1698f405752e
Manas Alok Awasthi <92290564+manasawasthi272@users.noreply.github.com>
2024-07-30 07:03:59 +0530

    Delete Backend directory

--- Backend/Bus-Scheduling-and-Booking-System/src/main/java/com/crimsonlogic/busschedulingandbookingsystem/service/DurationCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.crimsonlogic.busschedulingandbookingsystem.service;
-
-public class DurationCalculation {
-	public static int calculateDuration(int distance, int averageSpeed) {
-        return distance / averageSpeed;
-    }
-}
--- Backend/Bus-Scheduling-and-Booking-System/src/main/java/com/crimsonlogic/busschedulingandbookingsystem/service/FareCalculation.java
+++
@@ -1,6 +0,0 @@
-package com.crimsonlogic.busschedulingandbookingsystem.service;
-
-public class FareCalculation {
-	public static float calculateFare(float baseFarePerKm, float additionalCharges, int distance) {
-        return baseFarePerKm * distance + additionalCharges;
-    }}
